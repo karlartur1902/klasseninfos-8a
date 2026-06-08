8859181d1e4aee18bd5150a4f2313bd1cbf2f9b8
karlartur1902 <wegner.tka@gmail.com>
2026-06-08 11:37:16 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Klasseninfos 8A – zentrale Konfiguration
+// Nur diese Datei anpassen wenn sich die Apps Script Deployment-ID ändert.
+const API_URL = "https://script.google.com/macros/s/AKfycbyFC2I84QV0Y0NCxJnzoppkEDlddfWA6WNUYiNGrfkJCZR4Yk4XxteDountcfQpwvusUA/exec";
